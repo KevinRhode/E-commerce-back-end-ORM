2519c056dddcc522b3663a86dba6b7a7778b8e9e
Kevin Rhode <kevinrhode@hotmail.com>
2023-04-29 16:28:49 -0400

    add: POST category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -12,26 +12,63 @@ router.get('/', async (req, res) => {
       include:[Product]    
   
     }).catch((err) => {
-      res.status(404).json(err);
+      return res.status(404).json(err);
     });
-    res.status(200).json(categoryData);
+    return res.status(200).json(categoryData);
   } catch (error) {
-    res.status(500).json(error);
+    return res.status(500).json(error);
   }
   
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+
+    if (req.params.id) {
+      
+      const categoryData = await Category.findByPk(req.params.id,{
+        // attributes:[]
+        include:[Product],  
+        // where:{
+        //   id: req.params.id,
+        // }
+      }).catch((err) => {
+        return res.status(404).json(err);
+      });
+      return res.status(200).json(categoryData);
+
+    }
+
+    return res.status(400).send('Missing Id');
+
+    
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    if (req.body) {
+      const {category_name} = req.body;
+      // if (category_name) {
+        const resultData = await Category.create({category_name});
+      // }      
+      return res.status(200).json(resultData);
+    }
+    return res.status(400).send("Missing Request Body");
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  
 });
 
 router.delete('/:id', (req, res) => {
